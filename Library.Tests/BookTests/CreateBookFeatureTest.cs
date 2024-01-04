fff1b956418905dce27ad858d85b6393a638f0b4
Sérgio Ferreira <1221504@isep.ipp.pt>
2024-01-04 19:26:10 +0000

    Add CreateBookFeatureTest

---
+++ Library.Tests/BookTests/CreateBookFeatureTest.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Library.Application.Abstractions;
+using Library.Application.Features.Books.Commands;
+using Library.Domain.Abstractions;
+using Library.Domain.Entities;
+using Moq;
+
+namespace Library.Tests.BookTests
+{
+    public class CreateBookFeatureTest
+    {
+        private readonly Mock<IBookRepository> _bookRepository;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+        private readonly IMapper<CreateBookCommand, Book> _mapper;
+
+        public CreateBookFeatureTest()
+        {
+            _bookRepository = new Mock<IBookRepository>();
+            _unitOfWork = new Mock<IUnitOfWork>();
+            _mapper = new CreateBookMapper();
+        }
+
+        [Fact]
+        public async Task HandlerShouldReturnSuccess()
+        {
+            //Arrange 
+            var command = new CreateBookCommand
+            {
+                Author = "Shakes",
+                PublishDate = DateOnly.FromDateTime(new DateTime()),
+                Title = "Titulo",
+            };
+
+            var handler = new CreateBookCommandHandler(_bookRepository.Object, _unitOfWork.Object, _mapper);
+
+            _bookRepository.Setup(
+                x => x.AddBookAsync(
+                    It.IsAny<Book>(),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            //Act
+            var result = await handler.Handle(command, default);
+
+            //Assert
+            result.IsSuccess.Should().BeTrue();
+        }
+
+        [Fact]
+        public void MapShouldReturnSuccess()
+        {
+            //Arrange
+            var dateTimeNow = DateTime.Now.Date;
+
+            var bookCommand = new CreateBookCommand
+            {
+                Author = "Manuel",
+                PublishDate = DateOnly.FromDateTime(dateTimeNow),
+                Title = "Titulo" 
+            };
+
+            var expectedBook = new Book
+            {
+                Author = "Manuel",
+                PublishDate = dateTimeNow,
+                Title = "Titulo",
+            };
+
+            //Act
+            var bookMapped = _mapper.Map(bookCommand, new Book());
+
+            //Assert
+            bookMapped.Should().BeEquivalentTo(expectedBook);
+        }
+    }
+}
